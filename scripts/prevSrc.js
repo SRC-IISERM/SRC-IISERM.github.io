818fc03a668ceb4f772a8944be196219f055b595
Tanmay Pandey <tanmayfeb19@gmail.com>
2025-11-26 19:22:26 +0530

    SRC_PREFINAL_2

--- scripts/prevSrc.js
+++ scripts/prevSrc.js
@@ -5,7 +5,66 @@ const photo_src2022_2023 = "assets/src2022_2023";
 const photo_src2023_2024 = "assets/src2023_2024";
 const photo_src2024_2025 = "assets/src2024_2025"; // ← you must create this folder
 
+/**********************************
+ *    SRC 2024-2025
+ **********************************/
 
+const SRC2024_2025 = [{ name: "Soham Shannigrahi", role: "SRC Convenor", group: "core", meta: "MS22232", photo: "GEN00023 - Soham Shannigrahi.JPG" },
+    { name: "Kumar Twarit", role: "General Secretary", group: "core", meta: "MS23018", photo: "src photo - Kumar Twarit.jpg" },
+
+    // secretary
+    { name: "Swagotom Bhattacharjee", role: "Academic Secretary", group: "core", meta: "MS22112", photo: "IMG-20241012-WA0035 - Swagotom Bhattacharjee.jpg" },
+    { name: "Vishal Koli", role: "Cultural Secretary", group: "core", meta: "MS22175", photo: "-cpmvre - Vishal Vaibhav Koli.jpg" },
+    { name: "Arjun Kumar Sah", role: "Sports Secretary", group: "core", meta: "PH21099", photo: "Arjun - Arjun Kumar Sah.jpg" },
+
+    // hostel rep
+    { name: "Dhruv Kapoor", role: "H5 - Boys Rep", group: "hostel", meta: "MS22184", photo: "IMG_20240806_193108_609~2 - Dhruv Kapoor.jpg" },
+    { name: "Shraddha Agrawal", role: "H5 - Girls Rep", group: "hostel", meta: "MS22171", photo: "PXL_20230828_121733063.MP - Shraddha Agrawal.jpg" },
+    { name: "Koustav Maitra", role: "H6 - Boys Rep", group: "hostel", meta: "MS22146", photo: "" },
+    { name: "Deep Sehgal", role: "H7 - Boys Rep", group: "hostel", meta: "MS21022", photo: "Dance_Photo - Deep Sehgal.JPG" },
+    { name: "Sanjana Patil", role: "H7 - Girls Rep", group: "hostel", meta: "MS21201", photo: "" },
+    { name: "Mohak Sharma", role: "H8 - Boys Rep", group: "hostel", meta: "PH21012", photo: "" },
+    { name: "Simar Thind", role: "H8 - Girls Rep", group: "hostel", meta: "MS22048", photo: "IMG_20241221_105850 - Simar Thind.jpg" },
+    { name: "Pankaj Kumar", role: "H3 - Boys Rep", group: "hostel", meta: "MS22129", photo: "" },
+    { name: "Vedantika Dey", role: "H3 - Girls Rep", group: "hostel", meta: "MS23239", photo: "IMG_2099 - Vedantika Dey.jpeg" },
+    { name: "Sahil Deshmukh", role: "H4 - Boys Rep", group: "hostel", meta: "MS22233", photo: "IMG-20241112-WA0033~2 - Sahil Deshmukh.jpg" },
+    
+    // batch reps
+    { name: "Vishal Murali", role: "MS24 Batch Rep", group: "batch", meta: "MS24235", photo: "327c442f-d765-42ab-8b6e-4d30361fdff5 - VISHAL MURALI.jpeg" },
+    { name: "Divya Jyoti Ray", role: "MS24 Batch Rep", group: "batch", meta: "MS24056", photo: "IMG_20241208_140123_552 - DIVYA JYOTI RAY.webp" },
+    { name: "Agantuk Saha", role: "MS20 Thesis Rep", group: "batch", meta: "MS20076", photo: "Keyboard - AGANTUK SAHA.jpeg" },
+    { name: "Sharat Sapna Anuj", role: "MS20 Thesis Rep", group: "batch", meta: "MS20094", photo: "PXL_20241203_102212060 - SHARAT SAPNA ANUJ.jpg" },
+ 
+
+    //department rep
+    
+    { name: "Shruti Swaminathan", role: "MS22 Bio Dept. Rep", group: "department", meta: "MS22139", photo: "IMG_1620 - Shruti Swaminathan.jpeg" },
+    { name: "Tushti Govind", role: "MS23 Bio Dept. Rep", group: "department", meta: "MS23144", photo: "Tushti_MS23 - Tushti Govind.jpg" },
+
+    { name: "Dibyendu Sarkar", role: "MS22 Chem Dept. Rep", group: "department", meta: "MS22243", photo: "PHOTO - Dibyendu Sarkar.jpg" },
+    { name: "Sai Harshita", role: "MS23 Chem Dept. Rep", group: "department", meta: "MS23043", photo: "InCollage_20241219_204616684 - Sai Harshita.jpg" },
+
+    { name: "Kumar Twarit", role: "MS23 EES Dept. Rep", group: "department", meta: "MS23018", photo: "src photo - Kumar Twarit.jpg" },
+    
+    { name: "Aditya Bora", role: "MS22 Maths Dept. Rep", group: "department", meta: "MS22181", photo: "IMG-20241220-WA0025 - Aditya Sobham Bora.jpg" },
+    { name: "Archisha Mukherjee", role: "MS23 Maths Dept. Rep", group: "department", meta: "MS23201", photo: "" },
+    
+    { name: "Aditya Rao", role: "MS23 Phy Dept. Rep", group: "department", meta: "MS23195", photo: "IMG-20250101-WA0052~2 - Aditya Rao.jpg" },
+    
+    { name: "Arpit Awasthi", role: "EES PhD Rep", group: "department", meta: "PH22062", photo: "IMG-20240919-WA0044 - Arpit Awasthi.jpg" },
+    { name: "Aayush Gagneja", role: "HSS PhD Rep", group: "department", meta: "PH24011", photo: "" },
+    { name: "Shivika Gupta", role: "MS21 Bio Dept. Rep", group: "department", meta: "MS21173", photo: "PXL_20240415_150749162.MP - Shivika Gupta.jpg" },
+    { name: "Arnav Sinha", role: "MS21 Maths Dept. Rep", group: "department", meta: "MS21050", photo: "src - Arnav Sinha.jpeg" },
+    { name: "Shivam Kumar", role: "MS21 Chem Dept. Rep", group: "department", meta: "MS21084", photo: "IMG_20241219_223634 - Shivam Kumar.jpg" },
+    { name: "Subhamn Mahapatra", role: "MS21 Phy Dept. Rep", group: "department", meta: "MS21134", photo: "" },
+
+    { name: "Soham Shannigrahi", role: "MS22 Phy Dept. Rep", group: "department", meta: "MS22232", photo: "GEN00023 - Soham Shannigrahi.JPG" },
+    { name: "Vyshnav PT", role: "Int-PhD Maths Dept. Rep", group: "department", meta: "MP21013", photo: "" },
+    { name: "Kunal Banerjee", role: "Int-PhD Phy Dept. Rep", group: "department", meta: "MP21019", photo: "IMG-20241230-WA0004(1) - Kunal Banerjee.jpg" },
+    { name: "Sumonta Sarkar", role: "Int-PhD Chem Dept. Rep", group: "department", meta: "MP", photo: "" },
+    { name: "Sidhant Jakhar", role: "Phy PhD Rep", group: "department", meta: "PH20036", photo: "Sidhant_Jakhar - SIDHANT JAKHAR.jpg" },
+    { name: "Arun Kumar ", role: "Chem PhD Rep", group: "department", meta: "PH21104", photo: "IMG_5167 - Arun Kumar.jpeg" },
+];
 /**********************************
  *   SRC 2022–2023
  **********************************/
@@ -174,6 +233,7 @@ function setupSRCYear(blockId, members, photoDir) {
  * INIT
  ************************************/
 document.addEventListener("DOMContentLoaded", () => {
+  setupSRCYear("src2024_2025", SRC2024_2025, photo_src2024_2025);
   setupSRCYear("src2023_2024", SRC2023_2024, photo_src2023_2024);
   setupSRCYear("src2022_2023", SRC2022_2023, photo_src2022_2023);
 });
